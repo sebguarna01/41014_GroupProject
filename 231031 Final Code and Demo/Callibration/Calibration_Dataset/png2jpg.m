b451b96582514bb6bde2cc954e1c3475832d4cf5
Britney00 <55471488+Britney00@users.noreply.github.com>
2023-10-29 18:25:51 +1100

    Add files via upload
    Updating Calibration Dataset

--- 231031 Final Code and Demo/Callibration/Calibration_Dataset/png2jpg.m
+++ 231031 Final Code and Demo/Callibration/Calibration_Dataset/png2jpg.m
@@ -1,12 +1,16 @@
-%% Convert a .png image file type to a .jpg file type
+
+
+% Convert a .png image file type to a .jpg file type
 f = dir('*.png');
 
 fil = {f.name};
 
 for k = 1:numel(fil)
   file = fil{k};
-  new_file = strrep(strcat('I',file),'.png','.jpg');
+  new_file = strrep(strcat('calib_',file),'.png','.jpg');
   im = imread(file);
   imwrite(im, new_file);
 end
+
+
 
